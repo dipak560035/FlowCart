f61d672efb1ebd1b26c167a40473e1c8bdc4fb79
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-06-14 19:59:24 +0545

    Update accordion.tsx

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -1,29 +1,4 @@
 
-function AccordionContent({
-  className,
-  children,
-  ...props
-}: React.ComponentProps<typeof AccordionPrimitive.Content>) {
-  return (
-    <AccordionPrimitive.Content
-      data-slot="accordion-content"
-      className="overflow-hidden text-sm data-open:animate-accordion-down data-closed:animate-accordion-up"
-      {...props}
-    >
-      <div
-        className={cn(
-          "h-(--radix-accordion-content-height) pt-0 pb-2.5 [&_a]:underline [&_a]:underline-offset-3 [&_a]:hover:text-foreground [&_p:not(:last-child)]:mb-4",
-          className
-        )}
-      >
-        {children}
-      </div>
-    </AccordionPrimitive.Content>
-  )
-}
-
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
-
 function AccordionTrigger({
   className,
   children,
